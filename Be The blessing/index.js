e7e561e99a5ed9137895d2ba306081c08c91a071
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-07-19 20:31:01 -0700

    Toggle update

--- Be The blessing/index.js
+++ Be The blessing/index.js
@@ -11,13 +11,3 @@ let dropdown = document.querySelector(".dropdown-tabs");
 //   document.getElementById("myDropdown").classList.toggle("show");
 // })
 //   console.log("DKDKDK")
-
-// // Close the dropdown if the user clicks outside of it
-// window.onclick = function(e) {
-//   if (!e.target.matches('.dropbtn')) {
-//   var myDropdown = document.getElementById("myDropdown");
-//     if (myDropdown.classList.contains('show')) {
-//       myDropdown.classList.remove('show');
-//     }
-//   }
-// }

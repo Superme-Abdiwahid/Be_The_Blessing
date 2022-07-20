6e98d62e78bcaecabcba61761242d9bff087671a
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-07-19 20:29:01 -0700

    Initial commits

---
+++ Be The blessing/index.js
@@ -0,0 +1,23 @@
+let hamburgerMenu = document.querySelector("dropdown-toggle");
+let dropdown = document.querySelector(".dropdown-tabs");
+
+// hamburgerMenu.addEventListener("click", function() {
+//   dropdown.classList.toggle("hidden");
+// });
+
+// let i = document.querySelector("button");
+
+// i.addEventListener('click', () =>{
+//   document.getElementById("myDropdown").classList.toggle("show");
+// })
+//   console.log("DKDKDK")
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(e) {
+//   if (!e.target.matches('.dropbtn')) {
+//   var myDropdown = document.getElementById("myDropdown");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+// }

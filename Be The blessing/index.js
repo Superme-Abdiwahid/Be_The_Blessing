9f243c716ffaf3d3e74f9be2ed677d2b1739975c
ADIWAHID <abdiwahidhajir91@gmail.com>
2022-09-13 13:02:46 -0700

    be the blessing

--- Be The blessing/index.js
+++ Be The blessing/index.js
@@ -12,3 +12,38 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+            _docs_flag_cek = "F7xTd_RHMkj3mxWKA3pFRA\x3d\x3d";
+            if (window["DOCS_timing"]) {
+                DOCS_timing["ifdld"] = new Date().getTime();
+            }
+            function _DumpException(e) {
+                throw e;
+            }
+            DOCS_timing["cov"] = new Date().getTime();
